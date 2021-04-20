50106bb02fe221dc6a694a62e8ce4dbf39df5f87
Hugh Cavanagh <hacavanagh@gmail.com>
2021-04-20 16:32:55 +0100

    Refactor to have operands as keys of an object with the operation as the value

--- src/calculator.js
+++ src/calculator.js
@@ -1,24 +1,23 @@
 'use strict'
 
 function calculate(sumString) {
-  let numbers;
-  const operands = ['+', '-']
+  let numbers, returnArr;
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a,b) => a - b
+  }
 
   const separateOnOperand = (operand) => {
     return sumString.split(operand).map(n => parseInt(n.trim()))
   }
 
-  operands.forEach((operand) => {
+  Object.keys(operations).forEach((operand) => {
     if (sumString.includes(operand)) {
       numbers = separateOnOperand(operand);
+      returnArr = [sumString, operations[operand](numbers[0], numbers[1])]
     };
   });
 
-
-  if(sumString.includes('+')){
-    return [sumString, numbers[0] + numbers[1]]
-  } else if(sumString.includes('-')){
-    return [sumString, numbers[0] - numbers[1]]
-  }
+  return returnArr;
 
 }
