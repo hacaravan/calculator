ce9b35e530e319684dadb121ec33af81d86fef94
Hugh Cavanagh <hacavanagh@gmail.com>
2021-04-20 15:41:18 +0100

    Refactoring to remove unnecessary logs and shorten code

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,6 @@
 'use strict'
 
 function calculate(sumString) {
-  let numberStrings = sumString.split('+')
-  let numbers = numberStrings.map(n => parseInt(n.trim()))
-  console.log(numbers)
+  let numbers = sumString.split('+').map(n => parseInt(n.trim()))
   return [sumString, numbers[0] + numbers[1]]
 }
