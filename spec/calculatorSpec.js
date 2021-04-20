ee741762c8d4b3783c564f69570b51931fe1b1ac
Hugh Cavanagh <hacavanagh@gmail.com>
2021-04-20 15:45:55 +0100

    Calculator can subtract one number from another

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -7,4 +7,9 @@ describe('calculate', () => {
       expect(calculate('2 + 3')).toEqual(['2 + 3', 5])
     })
   })
+  describe('when given a simple subtraction of two numbers', () => {
+    it('returns the sum passed to it and the result of the sum', () => {
+      expect(calculate('1 - 1')).toEqual(['1 - 1', 0])
+    })
+  })
 })
--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,13 @@
 'use strict'
 
 function calculate(sumString) {
-  let numbers = sumString.split('+').map(n => parseInt(n.trim()))
-  return [sumString, numbers[0] + numbers[1]]
+  let numbers;
+  if(sumString.includes('+')){
+    numbers = sumString.split('+').map(n => parseInt(n.trim()))
+    return [sumString, numbers[0] + numbers[1]]
+  } else if(sumString.includes('-')){
+    numbers = sumString.split('-').map(n => parseInt(n.trim()))
+    return [sumString, numbers[0] - numbers[1]]
+  }
+
 }
