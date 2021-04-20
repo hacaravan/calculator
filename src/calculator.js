0ec145943cb70d9bb88dc64919d3c6e521f73dd0
Hugh Cavanagh <hacavanagh@gmail.com>
2021-04-20 16:22:08 +0100

    Refactor to loop through each operand and separate if it is included in the sum

--- src/calculator.js
+++ src/calculator.js
@@ -2,14 +2,22 @@
 
 function calculate(sumString) {
   let numbers;
-  const separateOnOperand = (string, operand) => {
+  const operands = ['+', '-']
+
+  const separateOnOperand = (operand) => {
     return sumString.split(operand).map(n => parseInt(n.trim()))
   }
+
+  operands.forEach((operand) => {
+    if (sumString.includes(operand)) {
+      numbers = separateOnOperand(operand);
+    };
+  });
+
+
   if(sumString.includes('+')){
-    numbers = separateOnOperand(sumString, '+')
     return [sumString, numbers[0] + numbers[1]]
   } else if(sumString.includes('-')){
-    numbers = separateOnOperand(sumString, '-')
     return [sumString, numbers[0] - numbers[1]]
   }
 
